414064ed59bb715e3fadb7bd120104dd3063b27e
muhammadimran81 <27628612+muhammadimran81@users.noreply.github.com>
2024-04-17 19:25:23 +0000

    Added HTML and CSS

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
 @app.route("/")
+
 def my_first_flask_app():
-    return "Test the flask web"
+    return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
