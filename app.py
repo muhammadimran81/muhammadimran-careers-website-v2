00c79958da52dfe1848d4cea78cbd6dd7cc366ab
muhammadimran81 <27628612+muhammadimran81@users.noreply.github.com>
2024-04-17 20:42:21 +0000

    added bootstrap

--- app.py
+++ app.py
@@ -2,10 +2,37 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+Jobs=[
+  {
+    'id':1,
+    'title':'Data Analyst',
+    'location':'Karachi, Pakistan',
+    'salary':'Rs. 10,00,000'
+  },
+  {
+    'id':2,
+    'title':'Data Scientist',
+    'location':'Islamabad, Pakistan',
+    'salary':'Rs. 15,00,000'
+  },
+  {
+    'id':3,
+    'title':'Frontend Engineer',
+    'location':'Remote',
+    'salary':'Rs. 12,00,000'
+  },
+  {
+    'id':4,
+    'title':'Backend Engineer',
+    'location':'Lahore, Pakistan',
+    'salary':'Rs. 13,00,000'
+  }
+]
+
 @app.route("/")
 
 def my_first_flask_app():
-    return render_template("home.html")
+    return render_template("home.html",jobs=Jobs,company_name="Soft Horizon")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
